6b02b84f78f10f943a8ed9516af22cc67389d801
Strelkatlet <ignatievwa@gmail.com>
2022-08-02 15:21:50 +0300

    Задание 2.3.1 + исправления

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,6 +1,7 @@
 package web.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "user")
@@ -70,4 +71,18 @@ public class User {
                 ", age=" + age +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName, age);
+    }
+
 }
